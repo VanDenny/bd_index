37c3818da83feec8321989eb5becc1b2ba2fd190
Van_Denny <575548935@qq.com>
2018-05-27 09:49:03 +0800

    完善功能

--- bdindex.py
+++ bdindex.py
@@ -4,6 +4,7 @@ import urllib
 import pandas as pd
 import os
 import pyautogui as pag
+from PIL import Image
 from selenium.webdriver import ActionChains
 from PIL import Image
 import pytesseract
@@ -68,11 +69,11 @@ class Bdindex_clawer:
         if self.cookie_valid:
             if os.path.exists('cookie.xlsx'):
                 self.cookie_list = pd.read_excel('cookie.xlsx').to_dict(orient='records')
-                print(self.cookie_list)
+                # print(self.cookie_list)
                 self.driver.delete_all_cookies()
                 for cookie in self.cookie_list:
                     if cookie['name'] in ['BDUSS', 'BAIDUID']:
-                        print(cookie)
+                        # print(cookie)
                         self.driver.add_cookie({'name': cookie['name'], 'value': cookie['value']})
             else:
                 print('找不到cookie')
@@ -85,31 +86,84 @@ class Bdindex_clawer:
         print(url)
         self.driver.get(url)
         current_url = self.driver.current_url
-        if current_url == url:
+        denny_pic = self.driver.find_elements_by_xpath('//img[@src="/static/imgs/deny.png"]')
+        if current_url == url and len(denny_pic) == 0:
             self.driver.maximize_window()
             time.sleep(2)
             sel = '//a[@rel="%s"]' % period
             self.driver.find_element_by_xpath(sel).click()
             self.driver.find_element_by_id('trend-meanline').click()
             time.sleep(2)
+            # rect = self.driver.find_element_by_xpath("//*[name()='svg']/*[name()='rect'][11]")
+            # rect_location = rect.location
+            # rect_size = rect.size
+            # print("location: %s, size: %s" % (rect_location, rect_size))
+            # ActionChains(self.driver).move_to_element(rect).click().perform()
             js = 'window.scrollTo(0,100);'
             self.driver.execute_script(js)
             time.sleep(1)
-            pag.moveTo(700, 800, 0.5)
+            pag.moveTo(245, 950, 0.5)
             pag.click()
-            # rect = self.driver.find_element_by_xpath("//*[name()='svg']/*[name()='rect'][3]")
-            # ActionChains(self.driver).move_to_element(rect).click().perform()
+            time.sleep(1)
+            content_word = self.driver.find_element_by_id("trendBarVal")
+            index_location = content_word.location
+            index_size = content_word.size
+            print("location: %s, size: %s" % (index_location, index_size))
             time.sleep(3)
             self.driver.save_screenshot('pic/%s_%s_%s.png' % (key_word, city, period))
+            img = Image.open('pic/%s_%s_%s.png' % (key_word, city, period))
+            # 跨浏览器兼容
+            # scroll = self.driver.execute_script("return window.scrollY;")
+            # print(scroll)
+            # top = index_location['y'] - scroll
+            # print(top)
+            rangle = (
+                int(index_location['x'] + 50),
+                int(index_location['y'] + 30),
+                int(index_location['x'] + index_size['width'] + 50),
+                int(index_location['y'] + index_size['height'] + 28)
+            )
+            img = img.crop(rangle)
+            x_rz = index_size['width'] * 3
+            y_rz = index_size['height'] * 3
+            img_rz = img.resize((x_rz, y_rz), Image.ANTIALIAS)
+            img_rz = img_rz.convert('RGB')
+            img_rz.save('pic/%s_%s_%s.jpg' % (key_word, city, period), quality=95)
+            index_dict = {}
+            index_dict['keyword'] = key_word
+            index_dict['city'] = city
+            index_dict['period'] = period
+            try:
+                code = pytesseract.image_to_string(img_rz)
+                if code:
+                    index_dict['index'] = code
+                else:
+                    index_dict['index'] = ''
+            except:
+                index_dict['index'] = ''
+            print(index_dict)
+            return index_dict
+        elif denny_pic:
+            time.sleep(20)
+            self.driver.delete_all_cookies()
+            self.get_index(province, city, key_word, period)
         else:
             Bdindex_clawer.cookie_valid = False
             self.get_index(province, city, key_word, period)
 
+
+
     def process(self):
         city_name = [i for i in self.city_code.keys()]
+        res_list =[]
         for ord, key_word in enumerate(city_name):
             for city in city_name[ord+1:]:
-                self.get_index('山东', city, key_word, 'all')
+                time.sleep(3)
+                res_list.append(self.get_index('山东', city, key_word, 'all'))
+        df = pd.DataFrame(res_list)
+        df.to_excel('result/shandong.xlsx')
+
+
 
 
 
